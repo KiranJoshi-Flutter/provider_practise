e1361fd669a3c03570c316132670dee23a719d96
kiranjoshi <keronz7777@gmail.com>
2022-04-27 14:56:07 +0545

    Initial commit

---
+++ android/app/src/main/kotlin/keronz/example/flutter_app_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package keronz.example.flutter_app_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_2/providers/counter_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => Counter(),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter App'),
+      ),
+      body: Center(
+        child: Text('Incrementing Counter : ${context.watch<Counter>().count}'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.read<Counter>().increment();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
